b6679b4a56b49ba4ddb1ee3ed619263d21758ac2
Dushes4 <85163946+Dushes4@users.noreply.github.com>
2022-12-07 22:26:57 +0300

    Create DROP.sql

---
+++ DROP.sql
@@ -0,0 +1,25 @@
+DROP INDEX name_index;
+DROP INDEX rent_user_index;
+DROP INDEX task_employee_index;
+DROP INDEX jobs_delivered_index;
+DROP INDEX jobs_solved_index;
+DROP INDEX task_scooter_t_index;
+DROP INDEX rent_scooter_index;
+DROP INDEX parkingh_scooter_index;
+
+DROP TABLE s309613.Task_scooter;
+DROP TABLE s309613.Task;
+DROP TABLE s309613.Rent;
+DROP TABLE s309613.Parking_history;
+DROP TABLE s309613.Parking;
+DROP TABLE s309613.Scooter;
+DROP TABLE s309613.Support_request;
+DROP TABLE s309613.Jobs_done;
+DROP TABLE s309613.Employee_movement;
+DROP TABLE s309613.Appointment;
+DROP TABLE s309613.Job;
+DROP TABLE s309613.Employee;
+DROP TABLE s309613."User";
+DROP TABLE s309613.Person;
+DROP TYPE JOB_TYPE_ENUM;
+
