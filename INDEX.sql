c00cef589dd7ae9618c94e8ffac3a215bcab7ac5
Dushes4 <85163946+Dushes4@users.noreply.github.com>
2022-12-11 23:27:09 +0300

    Update INDEX.sql

--- INDEX.sql
+++ INDEX.sql
@@ -1,8 +1,9 @@
 create index name_index on Person using hash(name);
-create index rent_user_index on Rent using hash(user_id);
-create index task_employee_index on Task using hash(employee_id);
-create index jobs_delivered_index on Jobs_done using btree(scooters_delivered);
-create index jobs_solved_index on Jobs_done using btree(requests_solved);
-create index task_scooter_t_index on Task_scooter using hash(task_id);
-create index rent_scooter_index on Rent using hash(scooter_id);
-create index parkingh_scooter_index on Parking_history using hash(scooter_id);
+create index name_index on Person using hash(surname);
+create index rent_user_index on Rent using btree(user_id);
+create index task_scooter_t_index on Task_scooter using btree(task_id);
+/*
+create index task_employee_index on Task using btree(employee_id);
+create index rent_scooter_index on Rent using btree(scooter_id);
+create index parkingh_scooter_index on Parking_history using btree(scooter_id);
+*/
